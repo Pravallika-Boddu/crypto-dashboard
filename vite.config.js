22c9a1748dd9907fbba3c34adb541ed9aa58d2eb
Pravallika-Boddu <boddupravallika04@gmail.com>
2025-09-19 09:29:35 +0530

    Fixing configuration issues

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  plugins: [react({
+    include: "**/*.{jsx,tsx}",
+  })],
+  base: "./",
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
