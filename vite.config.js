9cbba5b1dec8648010b2e61aaea90eb824708c3e
Pravallika-Boddu <boddupravallika04@gmail.com>
2025-09-19 09:45:12 +0530

    Deployment issues

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 
 export default defineConfig({
-  plugins: [react({
-    include: "**/*.{jsx,tsx}",
-  })],
-  base: "./",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, "index.html"),
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
-})
+  plugins: [react()],
+  base: './',  
+});
